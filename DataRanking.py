e22bbf110dadc65a83971e4e546ad19470905987
Alexandre José Ferreira <144146436+aleengaut@users.noreply.github.com>
2025-01-30 20:01:49 -0300

    Update DataRanking.py

--- DataRanking.py
+++ DataRanking.py
@@ -7,7 +7,7 @@ Created on Fri Jun 14 09:16:42 2024
 @licence: MIT
 
     CSV files must contain a header with the names of 
-        the criteria and, respectively, in the rows, ordinal 
+        the criteria and, respectively, in the rows (alternatives), ordinal 
         or cardinal numerical information. If in doubt, access
         the example data.csv
 
@@ -23,36 +23,30 @@ requirements : numpy, pandas, sklearn
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
-import rankability
 import evikor
 
-#file=open('data_with_alternatives.csv','r')
-file=open('study3.csv','r')
+#file=open('data.csv','r')
+#file=open('study3.csv','r')
+file=open('dataFromKBS.csv','r')
+
 df = pd.read_csv(file)
-alt = df.pop('alternatives')
+alt = df['alternatives']
+data = df.drop(['alternatives'], axis=1)
 
-'''
-PCA must be applied carefully. It should be applied when the \
-    rankability of the data is very low and if it is lower than \
-        the rankability of the PCs.
-'''
-#pca = PCA(n_components=2)
-#pca.fit(data.to_numpy())
-#print(pca.explained_variance_ratio_)
-#print(pca.singular_values_)
+pca = PCA(n_components=2)
+pca.fit(data.to_numpy())
+print(pca.explained_variance_ratio_)
+print(pca.singular_values_)
 
-#df = pd.DataFrame(pca.fit_transform(data), columns=['pc1', 'pc2'])
+datapca = pd.DataFrame(pca.fit_transform(data), columns=['pc1', 'pc2'])
 
-#w=[0.5, 0.5]
-#p=[0.25, 0.25, 0.25, 0.25] 
-#r = evikor.EvikorRanking(df = df, dfAlt = alt, weights=w, parameters=p)
-#r.__evikorRank__()
+weights=[0.5, 0.5]
+parameters=[0.25, 0.25, 0.25, 0.25] 
+r = evikor.EvikorRanking(data = datapca, alt = alt, w=weights, p=parameters)
+r.__ranking__()
 
-w=[0.5, 0.5]
-p=[0.25, 0.25, 0.25, 0.25]
-r = evikor.EvikorRanking(df = df, dfAlt = alt, weights=w, parameters=p)
-r.__evikorRankFullInfo__()
-print('Rankability index:', r.rankabilityIndex)
-print('Data:', r.dfFullInfo)
+print('rankability: ', r.rho)
+print('Data(0_1 reescaled criteria):')
+print(r.dataOutput)
 
 
