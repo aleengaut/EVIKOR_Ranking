0439ac5acef5cc86996cf8dc182e4ad6ec800918
Alexandre José Ferreira <144146436+aleengaut@users.noreply.github.com>
2025-01-30 20:02:07 -0300

    Update evikor.py

--- evikor.py
+++ evikor.py
@@ -6,14 +6,12 @@ Created on Thu Jun 27 00:28:23 2024
 """
 
 import numpy as np
+import pandas as pd
 from rankability import DataRankable
 import math
 
 @staticmethod    
 def normalizationDFtoMaxBeZero(df):
-
-    #colDim = self.numberOfAlternatives
-    #colDim = df.shape[0]
 
     for c in df.keys():
         max = df[c].max()
@@ -26,7 +24,20 @@ def normalizationDFtoMaxBeZero(df):
                                 /delta              
     return df
 
-def domEffRank2Generator(vec):
+def normalizationDFtoMin0Max1(df):
+    
+    for c in df.keys():
+        max = df[c].max()
+        min = df[c].min()
+        delta = max - min
+        
+        if delta == 0.0: delta = 1.0
+
+        df[c] = (df[c] - min*np.ones(df.shape[0])) \
+                                /delta              
+    return df
+
+def t2Generator(vec):
     
     odd = 0
     rank = []
@@ -48,9 +59,10 @@ def domEffRank2Generator(vec):
     			if p == 2:
     				rank.append(i)
     				p = 1
+    				
     			i = i + 2
     			
-    	if odd == 0 and freq != 0:
+    	if odd == 0 and freq > 0:
     		for f in range(freq):
     			if p == 1:
     				rank.append(i+1)
@@ -58,9 +70,9 @@ def domEffRank2Generator(vec):
     			if p == 2:
     				rank.append(i)
     				p = 1
+    
     			i = i + 2
-    		rank.append(i)
-            i = i + 1
+    			rank.append(i)
     			
     	if odd == 0 and freq == 0:
     		rank.append(i) 
@@ -73,18 +85,15 @@ class EvikorRanking(DataRankable):
     proposed by Alexandre and Amit.
     
     requirements : numpy
-    input: alternative-criteria dataframe WITHOUT alternative column
-            alternative column
-            weights array
-            parameters array
+    input: alternative-criteria dataframe AND alternative column name
         
     OBS: Each criterion can be a benefit or a cost. However, to specifically 
         analyze dominance and efficiency, increasing numerical order of the 
         criterion results in greater dominance, that is, the higher the value 
-        of the criterion the better (benefit maximization).
+        of the criterion the better.
     '''
     
-    def __init__(self, df = None, dfAlt = None, weights = None , parameters = None):
+    def __init__(self, data = None, alt = None, w = None , p = None):
         '''
         Parameters
         ----------
@@ -98,131 +107,93 @@ class EvikorRanking(DataRankable):
         None.
 
         '''
-     
-        super().__init__(df)
+        
+        # Normalizad criteria max value -> 1.
+        data=normalizationDFtoMin0Max1(data)
+        
+        super().__init__(data, alt)
         
         try:
-            for w in range(weights.__len__()):
-                float(weights[w])
-            for p in range(parameters.__len__()):
-                float(parameters[p])
+            for wj in range(w.__len__()):
+                float(w[wj])
+            for pi in range(p.__len__()):
+                float(p[pi])
         except TypeError:
             print('could not convert string to float!')
                 
-        if parameters[0] + \
-                parameters[1] + \
-                    parameters[2] + \
-                        parameters[3] != 1.0:
-            raise Exception('opss..sum constraint: sum of parameters must be 1.')
+        p=np.array(p)
+        p=p/p.sum()
+        self.p = p
         
-        if np.dot(np.ones(weights.__len__()), weights) != 1.0:
-            raise Exception('opss..sum constraint: sum of weights must be 1.')
-                
-        self.df = df.copy()
-        self.dfOriginal = df.copy()
+        w=np.array(w)
+        w=w/w.sum()
+        self.w = w
+        
         self.__rankabilityIndex__()
+        self.__sumDominanceVectorAllCriteria__()
         self.__sumDomEffVectorAllCriteria__()
-        self.df[dfAlt.name] = dfAlt
-        self.dfAltName = dfAlt.name
-        self.weights = weights
-        self.parameters = parameters
         
-    def __insertDomAndEffRanks__(self):
+    def __insertRankst__(self):
               
-        self.df = self.df.sort_values(['dominance', 'efficiency'], ascending = [False, False])
+        self.dataOutput = self.dataOutput.sort_values(['d', 'lambda'], ascending = [True, True])
         i=self.numberOfAlternatives
         rank=[]
-        for index in self.df.index:
+        for index in self.dataOutput.index:
             rank.append(i)
             i = i - 1
-        self.df['domEffRank'] = rank
-        self.df['domEffRank'].astype('int64')
+        self.dataOutput['t'] = rank
+        self.dataOutput['t'].astype('int64')
         
-        dominance = self.df['dominance'].astype('int64')
+        dominance = self.dataOutput['d'].astype('int64')
         countDominance = dominance.value_counts()
                 
-        self.df['domEffRank2'] = domEffRank2Generator(countDominance)
-        self.df['domEffRank2'] = -self.df['domEffRank2'] + \
+        self.dataOutput['t2'] = t2Generator(countDominance)
+        self.dataOutput['t2'] = -self.dataOutput['t2'] + \
             self.numberOfAlternatives + 1
-        self.df['domEffRank2'].astype('int64')
+        self.dataOutput['t2'].astype('int64')
    
-    def __normalizationDataToMax__(self):
-        alt = self.df[self.dfAltName]
-        self.df = self.df.drop(self.dfAltName, axis=1)
-        self.normalized_df = normalizationDFtoMaxBeZero(self.df)
-        self.df[alt.name] = alt
-        
-       # return self.dataNormalized 
-        
     def __weigthedSumMinmax__(self):
         
-        a = self.df['dominance']
-        b = self.df['efficiency']
-        t1 = self.df['domEffRank']
-        t2 = self.df['domEffRank2']
-        alt = self.df[self.dfAltName]
-        self.df = self.df.drop(a.name, axis=1)
-        self.df = self.df.drop(b.name, axis=1)
-        self.df = self.df.drop(t1.name, axis=1)
-        self.df = self.df.drop(t2.name, axis=1)
-        self.df = self.df.drop(self.dfAltName, axis=1)
         weightSum = np.zeros(self.numberOfAlternatives)
         minmax = np.zeros(self.numberOfAlternatives)
         s = np.zeros(self.numberOfAlternatives)
+        
+        # Normalizad criteria max value -> 0.
+        self.data = normalizationDFtoMaxBeZero(self.data)
         j = 0
-        for c in self.df.keys():
+        for c in self.data.keys():
             sr = s
-            s = (self.weights[j]*self.df[c].values)
+            s = (self.w[j]*self.data[c].values)
             minmax = np.maximum(sr, s)
             weightSum = weightSum + s
             j = j + 1
         
-        self.df['S'] = weightSum
-        self.df['R'] = minmax
-        self.df['dominance'] = a
-        self.df['efficiency'] = b
-        self.df['domEffRank'] = t1
-        self.df['domEffRank2'] = t2
-        self.df[self.dfAltName] = alt
+        self.dataOutput['S'] = weightSum
+        self.dataOutput['R'] = minmax
+
         
-    def __evikorRank__(self):
-        self.__insertDomAndEffRanks__()
-        self.__normalizationDataToMax__()
+    def __ranking__(self):
         self.__weigthedSumMinmax__()
+        self.__insertRankst__()
         
-        q = self.parameters[0]*self.df['S'] + \
-            self.parameters[1]*self.df['R'] + \
-            self.parameters[2]*self.df['domEffRank'] + \
-            self.parameters[3]*self.df['domEffRank2']
+        df = pd.DataFrame(data=self.dataOutput['S'])
+        df['R'] = self.dataOutput['R']
+        df['t'] = self.dataOutput['t']
+        df['t2'] = self.dataOutput['t2']
+        df = normalizationDFtoMin0Max1(df)
         
-        self.df['q'] = q
-        evikorRank = self.df['q'].rank()
+        k = 0
+        q = np.zeros(self.numberOfAlternatives)
+        for r in df.keys():
+            q = q + self.p[k]*df[r]
+            k = k + 1 
         
-        out = self.dfOriginal
-        out['evikorRank'] = evikorRank.astype('int64')
-        out[self.dfAltName] = self.df[self.dfAltName]
+        self.dataOutput['q'] = q
+        self.q = q
+        evikorRank = self.dataOutput['q'].rank()
         
-        self.df = self.df
-        self.df = self.dfOriginal
+        self.dataOutput['Ranking'] = evikorRank.astype('int64')
+        self.ranking = evikorRank
         
-        return out
-        
-    def __evikorRankFullInfo__(self):
-        self.__insertDomAndEffRanks__()
-        self.__normalizationDataToMax__()
-        self.__weigthedSumMinmax__()
-        self.__dominanceLevelVector__()
-        
-        q = self.parameters[0]*self.df['S'] + \
-            self.parameters[1]*self.df['R'] + \
-            self.parameters[2]*self.df['domEffRank'] + \
-            self.parameters[3]*self.df['domEffRank2']
-        
-        self.df['q'] = q
-        evikorRank = self.df['q'].rank()
-        
-        self.df['evikorRank'] = evikorRank.astype('int64')
-        
-        self.dfFullInfo = self.df
-        self.df = self.dfOriginal
-        
+        self.dataOutput['S'] = df['S']
+        self.dataOutput['R'] = df['R']
