5bd4dc65e6bc42aa0be94f32332de6f81c85368b
Alexandre José Ferreira <144146436+aleengaut@users.noreply.github.com>
2025-01-30 20:02:10 -0300

    Update rankability.py

--- rankability.py
+++ rankability.py
@@ -7,33 +7,62 @@ Created on Mon Mar 27 16:10:41 2023
 """
 
 import numpy as np
-from numpy.linalg import norm
-       
+
 class DataRankable():
     ''' Class for estimating data rankability using the rho method 
     proposed by Alexandre and Amit.
     
     requirements : numpy
-    input: alternative-criteria dataframe WITHOUT alternative column
-           alternative column
-
-     OBS: Each criterion can be a benefit or a cost. However, to specifically 
+    input: alternative-criteria dataframe WITHOUT alternative column name
+        
+    OBS: Each criterion can be a benefit or a cost. However, to specifically 
         analyze dominance and efficiency, increasing numerical order of the 
         criterion results in greater dominance, that is, the higher the value 
-        of the criterion the better (benefit maximization).
+        of the criterion the better.
     '''
     
-    def __init__(self, df = None, dfalt = None):
+    def __init__(self, data = None, alt = None):
         
-        for columnname in df.keys():
+        for columnname in data.keys():
             try:
-                df[columnname].astype(float)
+                data[columnname].astype(float)
             except TypeError:
                 print('could not convert string to float!')
                 
-        self.df = df
-        self.numberOfAlternatives = self.df.shape[0]
-        self.numberOfCriteria = self.df.shape[1]
+        self.data = data
+        self.alt = alt
+        self.numberOfAlternatives = self.data.shape[0]
+        self.numberOfCriteria = self.data.shape[1]
+        self.dataOutput = self.data.copy()
+        self.dataOutput['alt'] = self.alt
+ 
+    def __rankabilityIndex__(self):
+        '''
+
+        Returns
+        -------
+        rankability_index : rankability index estimation
+        '''
+        data = np.matrix(self.data)
+        rank = np.array(range(self.numberOfAlternatives))
+        rank = np.flip(rank)
+        
+        e = np.zeros(self.numberOfAlternatives)
+        for a in range(self.numberOfAlternatives):
+            for b in range(self.numberOfAlternatives):
+                test = 0
+                for c in range(self.numberOfCriteria):
+                    if data[a,c] > data[b,c]: test=test+1 
+                test = test // self.numberOfCriteria
+                e[a] = e[a] + test
+        
+        rho = e.sum()/rank.sum()
+        
+        self.rho = round(rho,4)
+        self.e = e
+        self.dataOutput['e'] = e.astype('int64')
+    
+        return round(rho,2), e
     
     def __completeDominanceVector__(self):
         
@@ -45,73 +74,58 @@ class DataRankable():
         completeDominanceVector : minimum non-dominance vector.
         TYPE: array
         '''
-        x = np.array(range(self.numberOfAlternatives))
-        x = np.flip(x)
-        x = [float(self.numberOfCriteria*xi + 0) for xi in x]
-        self.completeDominanceVector= np.array(x)
+        dc = np.array(range(self.numberOfAlternatives))
+        dc = np.flip(dc)
+        dc = [float(self.numberOfCriteria*dci + 0) for dci in dc]
+        self.dc = np.array(dc)
         
-        return self.completeDominanceVector
+        return dc
         
-    def __completeNonDominanceVector__(self):
-        ''' Minimum dominance strength or
-        set with no dominated alternative
-        
-        Returns
-        -------
-        completeNonDominanceVector : maximum non-dominance vector.
-        TYPE: array
-        '''
-        self.completeNonDominanceVector = np.ones(self.numberOfAlternatives)* \
-            self.__completeDominanceVector__().sum() \
-                    /self.numberOfAlternatives
-        
-        return self.completeNonDominanceVector
-
-    def __dominanceVector__(self, vec):
+    def __dominanceVector__(self, each_crtrion):
         '''
         Parameters
         ----------
-        vec : criterion.
+        each_crtrion : criterion.
 
         Returns
         -------
         dom_each_crtrion : dominance vector for each criterion.
         '''
-        dom_each_crtrion = np.zeros(self.numberOfAlternatives)
-        i=0;
-        for x in vec:
-            for y in vec:
-                if x>y: #=
-                    dom_each_crtrion[i]=dom_each_crtrion[i]; #OR don[i]=don[i]+x-y;
-                if x<y: #=
-                    dom_each_crtrion[i]=dom_each_crtrion[i]+1; #OR don[i]=don[i]+y-x;
-                i=i+1;
-            i=0;
+        d_each_crtrion = np.zeros(self.numberOfAlternatives)
+        i=0
+        for x in each_crtrion:
+            for y in each_crtrion:
+                if x>y:
+                    d_each_crtrion[i]=d_each_crtrion[i]
+                if x<y:
+                    d_each_crtrion[i]=d_each_crtrion[i]+1
+                i=i+1
+            i=0
             
-        return dom_each_crtrion
+        return d_each_crtrion
     
-    def __dominanceEfficiencyVector__(self, vec):
+    def __dominanceEfficiencyVector__(self, each_crtrion):
         '''
         Parameters
         ----------
-        vec : criterion.
+        each_crtrion : criterion.
 
         Returns
         -------
         eff_each_crtrion : dominance efficiency vector for each criterion.
         '''
-        eff_each_crtrion = np.zeros(self.numberOfAlternatives)
-        i=0;
-        for x in vec:
-            for y in vec:
-                if x>y: #=
-                    eff_each_crtrion[i]=eff_each_crtrion[i]; #OR don[i]=don[i]+x-y;
-                if x<y: #=
-                    eff_each_crtrion[i]=eff_each_crtrion[i]+y-x;
-                i=i+1;
-            i=0;
+        lambda_each_crtrion = np.zeros(self.numberOfAlternatives)
+        i=0
+        for x in each_crtrion:
+            for y in each_crtrion:
+                if x>y:
+                    lambda_each_crtrion[i]=lambda_each_crtrion[i]
+                if x<y:
+                    lambda_each_crtrion[i]=lambda_each_crtrion[i]+y-x
+                i=i+1
+            i=0
         
-        return eff_each_crtrion
+        return lambda_each_crtrion
     
     def __sumDominanceVectorAllCriteria__(self):
         '''
@@ -120,20 +134,14 @@ class DataRankable():
         dominanceVectorAllCriteria : dominance vector for all criteria.
         TYPE: array
         '''
-        dom = np.zeros(self.numberOfAlternatives)
-        for col in self.df.keys():
-        	dom = dom + self.__dominanceVector__(self.df[col])
+        d = np.zeros(self.numberOfAlternatives)
+        for each_crtrion in self.data.keys():
+        	d = d + self.__dominanceVector__(self.data[each_crtrion])
             
-        self.df['dominance'] = dom.astype('int64')
+        self.d = d
+        self.dataOutput['d'] = d.astype('int64')
         
-        # there is symmetry in the criteria space that's why sort and flip
-        dom = np.sort(dom)
-        dom = np.flip(dom)
-        if not dom.any(): 
-            dom=self.__completeNonDominanceVector__()
-        self.dominanceVectorAllCriteria = dom
-        
-        return self.dominanceVectorAllCriteria
+        return d
     
     def __sumDomEffVectorAllCriteria__(self):
         '''
@@ -141,65 +149,45 @@ class DataRankable():
         -------
         dominance_eff_vector : dominance efficiency vector for all criteria.
         '''
-        eff = np.zeros(self.numberOfAlternatives)
-        for col in self.df.keys():
-        	eff = eff + self.__dominanceEfficiencyVector__(self.df[col])
+        lmbda = np.zeros(self.numberOfAlternatives)
+        for each_crtrion in self.data.keys():
+        	lmbda = lmbda + self.__dominanceEfficiencyVector__(self.data[each_crtrion])
             
-        self.df['efficiency'] = eff
+        self.lmbda = np.round(lmbda,4)
+        self.dataOutput['lambda'] = np.round(lmbda,4)
         
-        # there is symmetry in the criteria space that's why sort and flip
-        eff = np.sort(eff)
-        eff = np.flip(eff)
-        if not eff.any(): 
-            eff=self.__completeNonDominanceVector__()
-        self.dominanceEffVectorAllCriteria = eff
+        return np.round(lmbda,4)
+    
+    def __dominanceYMatrixik__(self, each_crtrion):
         
-        self.df = self.df.sort_values(['dominance', 'efficiency'], ascending = [False, False])
+        Y=np.zeros((self.numberOfAlternatives, self.numberOfAlternatives), dtype=float)
+        i,k=0,0
+        for ai in each_crtrion:
+            for ak in each_crtrion:
+                if ai>ak: 
+                    Y[i][k]=Y[i][k]
+                if ai<ak: 
+                    Y[i][k]=1
+                i=i+1
+            i=0
+            k=k+1
         
-        return self.dominanceEffVectorAllCriteria
+        return Y
     
-    def __rankabilityIndex__(self):
-        '''
-        rescale 0-1 cosine similarity
-
-        Returns
-        -------
-        rankability_index : rankability index estimation
-        '''
-        a=self.__completeDominanceVector__()
-        b=self.__completeNonDominanceVector__()
-        c=self.__sumDominanceVectorAllCriteria__()
-        cos_sim_1 = np.dot(a, b) / (norm(a) * norm(b))
-        cos_sim_2 = np.dot(c, a) / (norm(c) * norm(a))
-        r = np.abs(cos_sim_2 - cos_sim_1) \
-                            /(1.0 - cos_sim_1)
-        self.rankabilityIndex = r
-    
-        return self.rankabilityIndex
-    
-    def __dominanceLevelVector__(self):
+    def __DMatrix__(self):
         '''
         Returns
         -------
         dominance_level : dominance levels where 0 is the Pareto level.
+        requirements : run 
+                        .__rankabilityIndex__() and 
+                        .__sumDomEffVectorAllCriteria__()
+            
         '''
-        self.df = self.df.sort_values(['dominance', 'efficiency'], ascending = [False, False])
-
-        # caso m << n e m >=2
-        level=0 # nivel de dominancia...faixa pareto
-        vec=[]
-        dom = self.dominanceVectorAllCriteria
-        m = self.numberOfCriteria 
-        for i in dom:
-            for j in dom:
-                if i-j >= m:
-                    level+=1
-                    
-            vec.append(level)
-            level=0
+        D=np.zeros((self.numberOfAlternatives, self.numberOfAlternatives), dtype=float)
+        for each_crtrion in self.data.keys():
+        	D = D + self.__dominanceYMatrixik__(self.data[each_crtrion])
         
-        self.df['level'] = vec
+        self.D = D
         
-        self.dominanceLevelVector = vec
-        
-        return self.dominanceLevelVector
+        return D
